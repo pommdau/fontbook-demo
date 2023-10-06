154affa5c6e4847a4bffe9b1ced8bd405aef266d
IKEH1024 <pondau1024@gmail.com>
2023-10-06 18:28:20 +0900

    initial commit

---
+++ FontBookDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FontBookDemo
+//
+//  Created by HIROKI IKEUCHI on 2023/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FontBookDemo/FontBookDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontBookDemoApp.swift
+//  FontBookDemo
+//
+//  Created by HIROKI IKEUCHI on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct FontBookDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
