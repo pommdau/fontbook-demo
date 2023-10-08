f7640ad33ed45ab7bdf869d853679ee1424ac200
IKEH1024 <pondau1024@gmail.com>
2023-10-08 13:26:13 +0900

    wip

--- FontBookDemo/ContentView.swift
+++ FontBookDemo/ContentView.swift
@@ -15,8 +15,39 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        .onAppear() {
+            print(fontDirectories)
+        }
         .padding()
     }
+}
+
+extension ContentView {
+    
+    func extractFontURLs(in directory: URL) -> [URL] {
+        guard let files = try? FileManager.default.contentsOfDirectory(
+            at: directory,
+            includingPropertiesForKeys: nil) else {
+            return []
+        }
+        let extensions = ["otf", "ttf", "ttc"]        
+        return files.filter { extensions.contains($0.pathExtension) }
+    }
+    
+    var fontDirectories: [URL] {
+        guard
+            let systemLibraryPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .systemDomainMask, false).first,
+            let generalLibraryPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .localDomainMask, false).first,
+            let userLibraryPath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true).first
+        else {
+            return []
+        }
+        
+        return [systemLibraryPath, generalLibraryPath, userLibraryPath]
+            .map { URL(filePath: $0) }
+            .map { $0.appendingPathComponent("Fonts") }
+    }
+    
 }
 
 #Preview {
